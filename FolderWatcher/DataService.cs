f9af45615e15ca8e3a05540c15cabeab230d64db
Aleksandr Ivakin <pwian@mail.ru>
2020-06-28 16:15:42 +0300

    add DataService

---
+++ FolderWatcher/DataService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FolderWatcher
+{
+    class DataService : IDataService
+    {
+        public void AddEvent(Event model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Event> GetEvent(string inName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FolderWatcher/IDataService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace FolderWatcher
+{
+    public interface IDataService
+    {
+        void AddEvent(Event model);
+        List<Event> GetEvent(string inName);
+    }
+}
